5db65f148fccb0744523e5eeeda9f3fdbde906eb
Arman <persakiss@gmail.com>
2022-06-01 14:27:48 +0400

    Header and heart component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
+import HeartLogo from './heart.svg';
 import './App.css';
 
-function App(props) {
-  return (
-    <div className="App">
-      {props.msg}
+const message = 'cool cud';
+
+const Heart = (prop) => {
+  return(
+    <div className='heart'>
+      <img className='heart-img' src={HeartLogo} alt='heart' />
+      <p className='heart-message'>{prop.msg}</p>
     </div>
-  );
+  )
+}
+
+const Header = (prop) => {
+  return(
+    <div className='App-header'>I Heart React</div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Heart msg={message} />
+    </>
+  )
 }
 
 export default App;
